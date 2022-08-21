e02d2f2fb8002b50d229a9e5a0e508a47b1243af
FullStackDevil <hique.rocha@gmail.com>
2022-08-20 21:37:40 -0300

    Good solution [1]

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,21 +9,19 @@ import model.entities.Reservation;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Scanner sc = new Scanner(System.in);
 
-		System.out.print("Room number: ");
-		int number = Integer.parseInt(sc.nextLine());
-		System.out.print("Check-in date (dd/MM/yyyy): ");
-		Date checkIn = sdf.parse(sc.next());
-		System.out.print("Check-out date (dd/MM/yyyy): ");
-		Date checkOut = sdf.parse(sc.next());
+		try {
+			System.out.print("Room number: ");
+			int number = Integer.parseInt(sc.nextLine());
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			Date checkIn = sdf.parse(sc.next());
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			Date checkOut = sdf.parse(sc.next());
 
-		if (!checkOut.after(checkIn)) {
-			System.out.println("Error in reservation: check-out date must be after check-in date");
-		} else {
 			Reservation res = new Reservation(number, checkIn, checkOut);
 			System.out.println("Reservation: " + res);
 
@@ -33,13 +31,14 @@ public class Program {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 
-			String error = res.updateDates(checkIn, checkOut);
-			if (error != null) {
-				System.out.println("Error in reservation: " + error);
-			} else {
-				System.out.println("Reservation: " + res);
-			}
+			res.updateDates(checkIn, checkOut);
+			System.out.println("Reservation: " + res);
+		} catch (ParseException e) {
+			System.out.println("Invalid date format");
+		} catch (IllegalArgumentException e) {
+			System.out.println("Error in reservation: " + e.getMessage());
 		}
+
 		sc.close();
 	}
 }
